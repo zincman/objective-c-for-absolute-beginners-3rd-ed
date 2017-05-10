df933980d4e40f359f1661438bf0640e0533a89d
Derek <lu.yidian@egolm.com>
2017-05-10 17:47:36 +0800

    none

---
+++ Chapter2/HelloWorld/HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Derek on 10/05/2017.
+//  Copyright © 2017 Derek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
