aac840d804711511d8bd860a460bec32b27e2cf7
Derek <lu.yidian@egolm.com>
2017-05-12 17:05:34 +0800

    finish chapter 7

---
+++ Chapter7/MyFirstApp/MyFirstApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApp
+//
+//  Created by Derek Lu on 11/05/2017.
+//  Copyright © 2017 Derek Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
