4c460a645243e39d33af808ea1d91ce5fcc64836
Derek <lu.yidian@egolm.com>
2017-05-13 16:17:33 +0800

    finish chapter 9

---
+++ Chapter9/Comparison/Comparison/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Comparison
+//
+//  Created by Derek on 13/05/2017.
+//  Copyright © 2017 Derek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chapter9/Comparison/Comparison/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Comparison
+//
+//  Created by Derek on 13/05/2017.
+//  Copyright © 2017 Derek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
